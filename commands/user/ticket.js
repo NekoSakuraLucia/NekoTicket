fc9838ce034b90c05c2da493cce00d3bbbd59f36
NekoSakuraLucia <mokurolucia@gmail.com>
2024-10-23 14:48:23 +0700

    เพิ่มโค้ดให้แสดงยืนยันเปิด ticket

--- commands/user/ticket.js
+++ commands/user/ticket.js
@@ -1,8 +1,30 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder().setName("ticket").setDescription("Open a ticket to report your server error or others."),
     async execute(interaction) {
-        await interaction.reply("Ticket")
+        await interaction.deferReply({ ephemeral: true })
+
+        // สร้าง embed สำหรับคำสั่ง ticket
+        const embed = new EmbedBuilder()
+        .setAuthor({ name: "NekoTicket", iconURL: interaction.user.displayAvatarURL() })
+            .setColor("LuminousVividPink")
+            .setTitle("Open Ticket !")
+            .setDescription("Are you sure you want to open ticket? Please confirm below.");
+
+        // สร้างปุ่มสำหรับการยืนยัน
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('confirm_ticket')
+                    .setLabel('Confirm')
+                    .setStyle(ButtonStyle.Primary),
+                new ButtonBuilder()
+                    .setCustomId('cancel_ticket')
+                    .setLabel('Cancel')
+                    .setStyle(ButtonStyle.Secondary),
+        );
+
+        await interaction.editReply({ embeds: [embed], components: [row] })
     }
 }
